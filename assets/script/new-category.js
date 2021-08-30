92a8db1fc84df868aedd763a1a9aaa163e8f83d1
grasielarosa <grasielarosaa@gmail.com>
2021-08-30 14:25:00 +0200

    add edit categories

--- assets/script/new-category.js
+++ assets/script/new-category.js
@@ -5,7 +5,7 @@ const categories = document.getElementById("categoriesId");
 // ***************** create edit y delete button *****************
 
 const createEditCategoryBtn = () => {
-  const btnEditCategory = document.createElement("button");
+  const btnEditCategory = document.createElement("a");
   btnEditCategory.innerText = "editar";
   btnEditCategory.setAttribute(
     "class",
@@ -26,72 +26,65 @@ const createDeleteCategoryBtn = () => {
   return btnDeleteCategory;
 };
 
-
 // ***************** create Category Object *****************
-const createCategoryObject = (
-    inputName
-) => {
-    
+const createCategoryObject = (inputName) => {
   const categoryObject = {
     id: createID(),
     name: inputName,
   };
-  
+
   const storageCategory = getStorage();
   storageCategory.categories.push(categoryObject);
   localStorage.setItem("ahorradas", JSON.stringify(storageCategory));
-  
+
   clearInputCategory();
   createNewCategory();
 };
 
-
 // ***************** create New Category *****************
 const createNewCategory = () => {
-    categories.innerHTML = " ";
-	const storage = getStorage();
-    
-    for (const category of storage.categories) {
-        const newList = createList();
-        newList.dataset.id = category.id;
-        const col1 = createDiv();
-        col1.setAttribute("class", "col-12 col-md-6 col-lg-8 align-self-md-end mb-2");
-      
-        const newCategory = document.createElement("span");
-        newCategory.innerText = category.name.toLowerCase();
-        newCategory.setAttribute("class", "bg-success px-lg-3 py-1");
-        newCategory.dataset.id = category.id;
+  categories.innerHTML = " ";
+  const storage = getStorage();
 
-        const col2 = createDiv();
-        col2.setAttribute(
-          "class",
-          "col-12 col-md-5 col-lg-3 text-md-end align-self-md-start me-0 me-md-3"
-        );
-        const newDeleteCategoryBtn = createDeleteCategoryBtn();
-        const newEditCategoryBtn = createEditCategoryBtn();
-      
-        col1.appendChild(newCategory);
-        col2.appendChild(newEditCategoryBtn);
-        col2.appendChild(newDeleteCategoryBtn);
-        newList.appendChild(col1);
-        newList.appendChild(col2);
-        categories.appendChild(newList);
-    }
-  
+  for (const category of storage.categories) {
+    const newList = createList();
+    newList.dataset.id = category.id;
+    const col1 = createDiv();
+    col1.setAttribute(
+      "class",
+      "col-12 col-md-6 col-lg-8 align-self-md-end mb-2"
+    );
 
-  
-  
+    const newCategory = document.createElement("span");
+    newCategory.innerText = category.name.toLowerCase();
+    newCategory.setAttribute("class", "bg-success px-lg-3 py-1");
+    newCategory.dataset.id = category.id;
+
+    const col2 = createDiv();
+    col2.setAttribute(
+      "class",
+      "col-12 col-md-5 col-lg-3 text-md-end align-self-md-start me-0 me-md-3"
+    );
+    const newDeleteCategoryBtn = createDeleteCategoryBtn();
+    const newEditCategoryBtn = createEditCategoryBtn();
+    newEditCategoryBtn.href = `./edit-categories.html?id=${category.id}&category=${category.name}`;
+
+    col1.appendChild(newCategory);
+    col2.appendChild(newEditCategoryBtn);
+    col2.appendChild(newDeleteCategoryBtn);
+    newList.appendChild(col1);
+    newList.appendChild(col2);
+    categories.appendChild(newList);
+  }
 };
 
 // ***************** delete function *****************
 const deleteCategory = (id) => {
-    const storage = getStorage();
-    const index = storage.categories.findIndex(el => el.id === id);
-    storage.categories.splice(index, 1);
-    localStorage.setItem("ahorradas", JSON.stringify(storage));
-    
-
-}
+  const storage = getStorage();
+  const index = storage.categories.findIndex((el) => el.id === id);
+  storage.categories.splice(index, 1);
+  localStorage.setItem("ahorradas", JSON.stringify(storage));
+};
 
 createNewCategory();
 
@@ -99,38 +92,32 @@ createNewCategory();
 // *****************       events      *****************
 // ***************** ***************** *****************
 
-
-
 // ***************** event with click *****************
 btnNewCategory.addEventListener("click", function () {
+  if (!inputName.value) return;
+  createCategoryObject(inputName.value);
+});
+
+// ***************** event with keyboard *****************
+inputName.addEventListener("keypress", function (e) {
+  if (e.keyCode === 13) {
     if (!inputName.value) return;
     createCategoryObject(inputName.value);
-    
-  });
-  
-  // ***************** event with keyboard *****************
-  inputName.addEventListener("keypress", function (e) {
-    if (e.keyCode === 13) {
-      if (!inputName.value) return;
-      createCategoryObject(inputName.value);
-    }
-  });
-  
-  // ***************** buttons event *****************
-  document.addEventListener("click", function (e) {
-      const el = e.target;
-      if (el.classList.contains("delete-category")) {
-      const li = el.parentElement.parentElement;
-      const id = li.getAttribute('data-id');
-      li.remove();
-      deleteCategory(id);
-      //! remover todas as operações
-      
-      }
-      if (el.classList.contains("edit-category")) {
-      const li = el.parentElement.parentElement;
-      const id = li.getAttribute('data-id');
-      //! editar categoria
-      console.log(id)
-      }
-  })
+  }
+});
+
+// ***************** buttons event *****************
+document.addEventListener("click", function (e) {
+  const el = e.target;
+  if (el.classList.contains("delete-category")) {
+    const li = el.parentElement.parentElement;
+    const id = li.getAttribute("data-id");
+    li.remove();
+    deleteCategory(id);
+    //! remover todas as operações
+  }
+  if (el.classList.contains("edit-category")) {
+    const li = el.parentElement.parentElement;
+    const id = li.getAttribute("data-id");
+  }
+});
